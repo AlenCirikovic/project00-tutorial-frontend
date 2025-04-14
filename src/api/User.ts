a068db66edde36f894b1849ff6254744f93c4f48
AlenCirikovic <alen.ciriko@gmail.com>
2025-04-14 18:11:57 +0200

    Created User hook 'useCreateUpdateUser' for Create and Update form aswell as added api requests for updating/creating/deleting and fetching all users.

--- src/api/User.ts
+++ src/api/User.ts
@@ -3,6 +3,7 @@ import { LoginUserFields } from 'hooks/react-hook-form/useLogin'
 import { UserType } from 'models/auth'
 import { RegisterUserFields } from 'hooks/react-hook-form/useRegister'
 import { apiRequest } from './Api'
+import { CreateUserFields, UpdateUserFields } from 'hooks/react-hook-form/useCreateUpdateUser'
 
 export const fetchUser = async () =>
   apiRequest<undefined, UserType>('get', apiRoutes.FETCH_USER)
@@ -21,4 +22,23 @@ export const uploadAvatar = async (formData: FormData, id: string) =>
     'post',
     `${apiRoutes.UPLOAD_AVATAR_IMAGE}/${id}`,
     formData,
+  )
+
+export const createUser = async (data:CreateUserFields)=>
+  apiRequest<CreateUserFields, void>('post',apiRoutes.USERS_PREFIX,data)
+
+export const updateUser = async (data:UpdateUserFields,id:string)=>
+  apiRequest<UpdateUserFields,void>(
+    'patch',
+    `${apiRoutes.USERS_PREFIX}/${id}`,
+    data,
+  )
+
+export const deleteUser = async (id:string) =>
+  apiRequest<string,UserType>('delete',`${apiRoutes.USERS_PREFIX}/${id}`)
+
+export const fetchUsers = async(pageNumber:number)=>
+  apiRequest<number,UserType[]>(
+    'get',
+    `${apiRoutes.FETCH_USER}?page=${pageNumber}`
   )
